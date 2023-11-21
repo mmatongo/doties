444c118a2de928b4b8a57ce95bfd756e20fd57db
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-11-21 11:02:07 +0000

    updates to tree colours

--- lua/plugins/nvim-tree-config.lua
+++ lua/plugins/nvim-tree-config.lua
@@ -43,12 +43,12 @@ require('nvim-tree').setup {
 -- Setup nvim-web-devicons
 require('nvim-web-devicons').setup {
   default = true;
-  color_icons = false;
+  color_icons = true;
 }
 
 vc [[
   autocmd Colorscheme * highlight NvimTreeNormal guibg=#030303
-  autocmd Colorscheme * highlight NvimTreeFolderName guifg=#d7ebba
+  autocmd Colorscheme * highlight NvimTreeFolderName guifg=#5FD7A7
   autocmd Colorscheme * highlight NvimTreeRootFolder guifg=#404040
   autocmd Colorscheme * highlight NvimTreeExecFile guifg=#d7ebba
   autocmd Colorscheme * highlight NvimTreeNormal guifg=#c5c8c6
@@ -56,7 +56,7 @@ vc [[
 ]]
 
 vc [[
-  highlight NvimTreeFolderIcon guifg=#c5c8c6
+  highlight NvimTreeFolderIcon guifg=#E32791
   highlight NvimTreeGitDirty guifg=#c5c8c6
   highlight NvimTreeGitStaged guifg=#c5c8c6
   highlight NvimTreeGitMerge guifg=#c5c8c6
