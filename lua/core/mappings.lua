3eb62d516fbd8d15514ba8615770c9a35350935d
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-03-07 23:06:03 +0000

    update mappings and make config cleaner

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -7,61 +7,50 @@ local vc = vim.cmd
 g.mapleader = ","
 
 -- keymaps
-vkm("n", "<C-s>", ":write<CR>", { noremap = true, silent = true })
-vkm("n", "<C-q>", ":q!<CR>", { noremap = true, silent = true })
-vkm("n", "<C-x>", ":quit<CR>", { noremap = true, silent = true })
-vkm("n", "<C-e>", ":lua minimal()<CR>", { noremap = true, silent = true })
-vkm("n", "<C-a>", ":tabnew<CR>", { noremap = true, silent = true })
-vkm("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-vkm("n", "<TAB>", ":tabnext<CR>", { noremap = true, silent = true })
-vkm("n", "<C-k>", ":ALEFix<CR>", { noremap = true, silent = true })
-vkm("n", "<C-v>", ":Vista!!<CR>", { noremap = true, silent = true })
+opts = { noremap = true, silent = true }
 
--- which-key
-vkm("n", "<C-]>", ":WhichKey<CR>", { noremap = true, silent = true })
+vkm("n", "<C-s>", ":write<CR>", opts)
+vkm("n", "<C-q>", ":q!<CR>", opts)
+vkm("n", "<C-x>", ":quit<CR>", opts)
+vkm("n", "<C-e>", ":lua minimal()<CR>", opts)
+vkm("n", "<C-a>", ":tabnew<CR>", opts)
+vkm("n", "<C-n>", ":NvimTreeToggle<CR>", opts)
+vkm("n", "<TAB>", ":tabnext<CR>", opts)
+vkm("n", "<C-k>", ":ALEFix<CR>", opts)
+vkm("n", "<C-v>", ":Lengendary.find<CR>", opts)
 
 -- telescope
-vkm("n", "<C-f>", ":Telescope find_files<CR>", { noremap = true, silent = true })
-vkm("n", "<C-g>", ":Telescope live_grep<CR>", { noremap = true, silent = true })
-vkm("n", "<C-w>", ":Telescope file_browser<CR>", { noremap = true, silent = true })
-vkm("n", "<C-h>", ":Telescope help_tags<CR>", { noremap = true, silent = true })
-vkm("n", "<C-p>", ":Telescope git_files<CR>", { noremap = true, silent = true })
-vkm("n", "<C-t>", ":Telescope treesitter<CR>", { noremap = true, silent = true })
-vkm("n", "<C-b>", ":Telescope buffers<CR>", { noremap = true, silent = true })
-vkm("n", "<C-c>", ":Telescope commands<CR>", { noremap = true, silent = true })
-vkm("n", "<C-u>", ":Telescope undo<CR>", { noremap = true, silent = true })
-
--- treesitter
---vkm("n", "<C-=", ":TSBufEnable highlight<CR>", { noremap = true, silent = true })
+vkm("n", "<C-f>", ":Telescope find_files<CR>", opts)
+vkm("n", "<C-g>", ":Telescope live_grep<CR>", opts)
+vkm("n", "<C-w>", ":Telescope file_browser<CR>", opts)
+vkm("n", "<C-h>", ":Telescope help_tags<CR>", opts)
+vkm("n", "<C-p>", ":Telescope git_files<CR>", opts)
+vkm("n", "<C-t>", ":Telescope treesitter<CR>", opts)
+vkm("n", "<C-b>", ":Telescope buffers<CR>", opts)
+vkm("n", "<C-c>", ":Telescope commands<CR>", opts)
+vkm("n", "<C-u>", ":Telescope undo<CR>", opts)
 
 -- other.nvim
---vkm("n", "<C-j>", ":OtherVSplit<CR>", { noremap = true, silent = true })
+vkm("n", "<C-j>", ":OtherVSplit<CR>", opts)
 
 -- mason
-vkm("n", "<C-[>", ":Mason<CR>", { noremap = true, silent = true })
+vkm("n", "<C-[>", ":Mason<CR>", opts)
 
--- sidebar
+-- lspsaga
+vkm("n", "gh", ":Lspsaga lsp_finder<CR>", opts)
+vkm("n", "gg", ":Lspsaga code_action<CR>", opts)
+vkm("n", "gj", ":Lspsaga hover_doc<CR>", opts)
+vkm("n", "gk", ":Lspsaga signature_help<CR>", opts)
+vkm("n", "gl", ":Lspsaga show_line_diagnostics<CR>", opts)
+vkm("n", "gq", ":Lspsaga rename<CR>", opts)
+vkm("n", "gR", ":Lspsaga preview_definition<CR>", opts)
 
 -- clear enter mapping
-vkm("n", "<CR>", "<NOP>", { noremap = true, silent = true })
-vkm("n", "<ESC>", "<NOP", { noremap = true, silent = true })
+vkm("n", "<CR>", "<NOP>", opts)
+vkm("n", "<ESC>", "<NOP", opts)
 
--- vim.lsp
---vkm("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
---vkm("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
---vkm("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
---vkm("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
---vkm("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
---vkm("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
---vkm(
---	"n",
---	"<leader>wl",
---	"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
---	{ noremap = true, silent = true }
---)
--- lua vim.lsp.buf.code_action()
---vkm("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
---vkm("n", "<C-l>", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
+-- toggleterm
+vkm("n", "<C-l>", ":ToggleTerm<CR>", opts)
 
 function minimal()
 	if active then
@@ -77,4 +66,13 @@ function minimal()
     ]])
 		active = true
 	end
+end
+
+-- check if neovide is running
+if g.neovide then
+	g.neovide_input_use_logo = 1
+	vkm('', '<D-v>', '+p<CR>', opts)
+	vkm('!', '<D-v>', '<C-R>+', opts)
+	vkm('t', '<D-v>', '<C-R>+', opts)
+	vkm('v', '<D-v>', '<C-R>+', opts)
 end
