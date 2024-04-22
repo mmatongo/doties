e7697e96de3d9e3d6c96866cd7ebd9692cfba635
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-04-22 20:42:45 +0100

    defer structrue

--- lua/plugins/lang/structrue-go-config.lua
+++ lua/plugins/lang/structrue-go-config.lua
@@ -1,66 +1,63 @@
-local present, structrue = pcall(require, "structrue-go")
+-- structrue-go config
 
-if not present then
-    return
-end
-
-structrue.setup({
+options = {
 	show_others_method = true, -- bool show methods of struct whose not in current file
 	show_filename = true, -- bool
 	number = "no", -- show number: no | nu | rnu
 	fold_open_icon = " ",
 	fold_close_icon = " ",
 	cursor_symbol_hl = "guibg=Gray guifg=White", -- symbol hl under cursor,
-	indent = "┠",  -- Hierarchical indent icon, nil or empty will be a tab
+	indent = "┠", -- Hierarchical indent icon, nil or empty will be a tab
 	position = "botright", -- window position,default botright,also can set float
 	symbol = { -- symbol style
+	-- TODO: update the colour scheme to use the same colours as the rest of the theme
 		filename = {
-		    hl = "guifg=#CCCCCC", -- highlight symbol
-		    icon = " " -- symbol icon
+			hl = "guifg=#CCCCCC", -- highlight symbol
+			icon = " ", -- symbol icon
 		},
 		package = {
-		    hl = "guifg=#008EC4",
-		    icon = " "
+			hl = "guifg=#008EC4",
+			icon = " ",
 		},
 		import = {
-		    hl = "guifg=#008EC4",
-		    icon = " ◈ "
+			hl = "guifg=#008EC4",
+			icon = " ◈ ",
 		},
 		const = {
-		    hl = "guifg=#D75F5F",
-		    icon = " π ",
+			hl = "guifg=#D75F5F",
+			icon = " π ",
 		},
 		variable = {
-		    hl = "guifg=#10A778",
-		    icon = " ◈ ",
+			hl = "guifg=#10A778",
+			icon = " ◈ ",
 		},
 		func = {
-		    hl = "guifg=#C30771",
-		    icon = "  ",
+			hl = "guifg=#C30771",
+			icon = "  ",
 		},
 		interface = {
-		    hl = "guifg=#CCCCCC",
-		    icon = "❙ "
+			hl = "guifg=#CCCCCC",
+			icon = "❙ ",
 		},
 		type = {
-		    hl = "guifg=#CCCCCC",
-		    icon = "▱ ",
+			hl = "guifg=#CCCCCC",
+			icon = "▱ ",
 		},
 		struct = {
-		    hl = "guifg=#CCCCCC",
-		    icon = "❏ ",
+			hl = "guifg=#CCCCCC",
+			icon = "❏ ",
 		},
 		field = {
-		    hl = "guifg=#E5E5E5",
-		    icon = " ▪ "
+			hl = "guifg=#E5E5E5",
+			icon = " ▪ ",
 		},
 		method_current = {
-		    hl = "guifg=#E5E5E5",
-		    icon = " ƒ "
+			hl = "guifg=#E5E5E5",
+			icon = " ƒ ",
 		},
 		method_others = {
-		    hl = "guifg=#E5E5E5",
-		    icon = "  "
+			hl = "guifg=#E5E5E5",
+			icon = "  ",
 		},
 	},
 	keymap = {
@@ -71,7 +68,7 @@ structrue.setup({
 		fold_toggle = "\\z",
 		refresh = "R", -- refresh symbols
 		preview_open = "P", -- preview  symbol context open
-		preview_close = "\\p" -- preview  symbol context close
+		preview_close = "\\p", -- preview  symbol context close
 	},
 	fold = { -- fold symbols
 		import = true,
@@ -81,5 +78,4 @@ structrue.setup({
 		interface = false,
 		func = false,
 	},
-})
-
+}
