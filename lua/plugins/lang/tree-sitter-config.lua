dcfd314d5953477b818933961f9bce8a68d2c938
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-04-22 20:44:08 +0100

    more inspiration

--- lua/plugins/lang/tree-sitter-config.lua
+++ lua/plugins/lang/tree-sitter-config.lua
@@ -1,12 +1,6 @@
 -- treesitter config
 
-local present, ts_config = pcall(require, "nvim-treesitter.configs")
-
-if not present then
-	return
-end
-
-ts_config.setup({
+options = {
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
@@ -39,4 +33,48 @@ ts_config.setup({
 	sync_install = false,
 	auto_install = true,
 	ignore_install = { "javascript" },
-})
+
+	highlight = {
+		enable = true,
+		disable = {
+			"css",
+			"scss",
+		},
+		additional_vim_regex_highlighting = false,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				["is"] = { query = "@statement.inner", desc = "statement" },
+				["as"] = { query = "@statement.outer", desc = "statement" },
+				["ic"] = { query = "@class.inner", desc = "class" },
+				["ac"] = { query = "@class.outer", desc = "class" },
+				["iF"] = { query = "@function.inner", desc = "function" },
+				["aF"] = { query = "@function.outer", desc = "function" },
+			},
+			selection_modes = {
+				["@parameter.outer"] = "v",
+				-- ["@function.outer"] = "V",
+				["@class.outer"] = "<c-v>",
+			},
+		},
+		swap = {
+			enable = true,
+		},
+		move = {
+			enable = true,
+			goto_next_start = {
+				["]f"] = { query = "@function.outer", desc = "function" },
+				["]c"] = { query = "@call.outer", desc = "call" },
+			},
+			goto_previous_start = {
+				["[f"] = { query = "@function.outer", desc = "function" },
+				["[c"] = { query = "@call.outer", desc = "call" },
+			},
+		},
+	},
+}
+
+return options
