fc666cf89c42bba4d571e6c6b8581495c42910cb
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-04-22 20:35:47 +0100

    update nvim-autopairs config

--- lua/plugins/editor/nvim-autopairs-config.lua
+++ lua/plugins/editor/nvim-autopairs-config.lua
@@ -1,13 +1,9 @@
 -- nvim-autopairs config
 
-local present, autopairs = pcall(require, "nvim-autopairs")
-
-if not present then
-	return
-end
-
-autopairs.setup({
+options = {
 	-- ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
 	ignored_next_char = "",
 	map_c_w = true,
-})
+}
+
+return options
