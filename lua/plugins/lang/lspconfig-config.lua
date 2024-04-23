789bdd1f1002abd47a69fc682976aa6f2182e0b1
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-04-23 10:58:56 +0100

    lsp_sig is handled elsewhere

--- lua/plugins/lang/lspconfig-config.lua
+++ lua/plugins/lang/lspconfig-config.lua
@@ -54,17 +54,17 @@ lspconfig.gopls.setup({
 	},
 })
 
-local status_ok, lsp_signature = pcall(require, "lsp_signature")
-if not status_ok then
-	return
-end
+-- local status_ok, lsp_signature = pcall(require, "lsp_signature")
+-- if not status_ok then
+-- 	return
+-- end
 
-lsp_signature.setup({
-	bind = true, -- This is mandatory, otherwise border config won't get registered.
-	handler_opts = {
-		border = "rounded",
-	},
-})
+-- lsp_signature.setup({
+-- 	bind = true, -- This is mandatory, otherwise border config won't get registered.
+-- 	handler_opts = {
+-- 		border = "rounded",
+-- 	},
+-- })
 
 lspconfig.rust_analyzer.setup({
 	capabilities = capabilities,
