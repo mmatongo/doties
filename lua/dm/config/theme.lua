c7e521543269fe2a51a9180591ecbdc150d0178a
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-06-11 23:52:22 +0100

    revert theme

--- lua/dm/config/theme.lua
+++ lua/dm/config/theme.lua
@@ -1,13 +1,11 @@
 local vc = vim.cmd
 return {
-	-- "rose-pine/neovim",
-    "Yazeed1s/oh-lucy.nvim",
+	"rose-pine/neovim",
 	-- event = "VeryLazy",
 	-- lazy = true,
 	priority = 1000,
 	opts = {},
 	config = function()
-		-- vc("colorscheme rose-pine")
-        vc("colorscheme oh-lucy")
+		vc("colorscheme rose-pine")
 	end,
 }
