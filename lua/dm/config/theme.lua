f8c1409eafe849965994806bf838537232e319a5
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-06-11 23:00:36 +0100

    change theme

--- lua/dm/config/theme.lua
+++ lua/dm/config/theme.lua
@@ -1,11 +1,13 @@
 local vc = vim.cmd
 return {
-	"rose-pine/neovim",
+	-- "rose-pine/neovim",
+    "Yazeed1s/oh-lucy.nvim"
 	-- event = "VeryLazy",
 	-- lazy = true,
 	priority = 1000,
 	opts = {},
 	config = function()
-		vc("colorscheme rose-pine")
+		-- vc("colorscheme rose-pine")
+        vc("colorscheme oh-lucy")
 	end,
 }
